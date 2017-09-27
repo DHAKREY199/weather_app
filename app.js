f8d9f88b95d2a765c1656c0bacd1f83ada7ec070
Himanshu Dhakrey <himanshudhakrey2014@gmail.com>
2017-09-27 05:48:06 +0000

    skycons problem solved

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ app.use(express.static("images"));
 const fToC = require('f-c');
 var options = {
   provider: 'google',
- 
-  // Optional depending on the providers 
   httpAdapter: 'https', // Default 
   apiKey: 'AIzaSyBKHRSwO5NRBz_m_Z9B9IJhGax9E9eqFOw', // for Mapquest, OpenCage, Google Premier 
   formatter: null         // 'gpx', 'string', ... 
@@ -48,22 +46,36 @@ app.get("/results", function(req, resp){
             //console.log(res);
             latitude = res[0].latitude;
             longitude = res[0].longitude;
+            console.log(JSON.stringify(res, undefined, 2));
             var url = "https://api.darksky.net/forecast/218682c0e0714e069de1b9c5ab44f6ed/"+latitude+","+longitude;
                     request(url,function(error, response, body){
                  if(!error&&response.statusCode == 200){
                 var data = JSON.parse(body);
-                //console.log(timeConverter(data.currently.time));
-              var obj = {
-                    query : query,
-                    current_time : {time : timeConverter(data.currently.time), temp : Math.round(fToC(data.currently.temperature))},
-                    other_time : [{time : timeConverter(data.hourly.data[0].time),temp : Math.round(fToC(data.hourly.data[0].temperature)), icon : data.hourly.data[0].precipType},
-                                  {time : timeConverter(data.hourly.data[1].time),temp : Math.round(fToC(data.hourly.data[1].temperature)), icon : data.hourly.data[1].precipType},
-                                  {time : timeConverter(data.hourly.data[5].time),temp : Math.round(fToC(data.hourly.data[5].temperature)), icon : data.hourly.data[5].precipType},
-                                  {time : timeConverter(data.hourly.data[7].time),temp : Math.round(fToC(data.hourly.data[7].temperature)), icon : data.hourly.data[7].precipType},
-                                  {time : timeConverter(data.hourly.data[9].time),temp : Math.round(fToC(data.hourly.data[9].temperature)), icon : data.hourly.data[9].precipType},
-                                  {time : timeConverter(data.hourly.data[10].time),temp : Math.round(fToC(data.hourly.data[10].temperature)), icon : data.hourly.data[10].precipType}],
-                                  
-              };
+                
+                var obj = {
+                query : query,
+                current : data.currently,
+                hourly : data.hourly,
+                timeConverter : function (UNIX_timestamp) {
+                      var a = new Date(UNIX_timestamp * 1000 + 1.98e+7);
+                      var months = ['Jan','Feb','Mar','Apr','May','Jun','July','Aug','Sep','Oct','Nov','Dec'];
+                      var year = a.getFullYear();
+                      var month = months[a.getMonth()];
+                      var date = a.getDate();
+                      var hour = a.getHours();
+                      var min = a.getMinutes();
+                      var sec = a.getSeconds();
+                      var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+                      if(hour>12){
+                        time = (hour-12) + ':' + "00"+' PM';
+                      }
+                      else
+                        time = hour + ':' + "00"+' AM';
+                      
+                      return time;
+                    
+                }
+              }
                 console.log(Math.round(fToC(data.currently.temperature)));
                 //console.log(data);
                 resp.render("results", {obj : obj});
